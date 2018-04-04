f320cae5c7408ef986a5b1c3b85a8b8334cda338
Clemens Hopfer <spam-datacop@wireloss.net>
2018-04-04 20:05:37 +0200

    css fix and bundle

--- conf.py
+++ conf.py
@@ -9,7 +9,8 @@ SITE_URL = "/"
 BLOG_EMAIL = "root@localhost"
 BLOG_DESCRIPTION = "Chaos Computer Club Wien"  # (translatable)
 THEME = 'c3w-eh18'
-USE_BUNDLES = False
+USE_BUNDLES = True
+USE_CDN = False
 
 TWITTER_CARD = {
 	'use_twitter_cards': True,
@@ -87,7 +88,7 @@ STRIP_INDEXES = True
 PRETTY_URLS = True
 COPY_SOURCES = False
 MARKDOWN_EXTENSIONS = ['fenced_code', 'codehilite', 'extra']
-UNSLUGIFY_TITLES = True
+FILE_METADATA_UNSLUGIFY_TITLES = True
 ENABLE_AUTHOR_PAGES = False
 COMMENT_SYSTEM = False
 INDEX_PATH = "aktuelles"
